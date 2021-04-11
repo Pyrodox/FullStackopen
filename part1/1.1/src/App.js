5747db38eae862a496554e9f756eafe4dcb39b99
Pyro-Impact <77907654+Pyro-Impact@users.noreply.github.com>
2021-04-10 22:41:45 -0700

    1.3-1.5

--- part1/1.1/src/App.js
+++ part1/1.1/src/App.js
@@ -1,13 +1,23 @@
 import React from 'react'
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   const Header = (props) => {
     return (
@@ -18,32 +28,11 @@ const App = () => {
   }
 
   const Content = (props) => {
-    const Partone = (props2) => {
-      return (
-        <div>
-          <p>{props2.parto}: {props2.ex1}</p>
-        </div>
-      )
-    }
-    const Parttwo = (props2) => {
-      return (
-        <div>
-          <p>{props2.parttw}: {props2.ex2}</p>
-        </div>
-      )
-    }
-    const Partthree = (props2) => {
-      return (
-        <div>
-          <p>{props2.partth}: {props2.ex3}</p>
-        </div>
-      )
-    }
     return (
       <div>
-        <Partone parto = {part1} ex1 = {exercises1}/>
-        <Parttwo parttw = {part2} ex2 = {exercises2}/>
-        <Partthree partth = {part3} ex3 = {exercises3}/>
+        <p>{course.parts[0].name}: {course.parts[0].exercises}</p>
+        <p>{course.parts[1].name}: {course.parts[1].exercises}</p>
+        <p>{course.parts[2].name}: {course.parts[2].exercises}</p>
       </div>
     )
   }
@@ -58,9 +47,9 @@ const App = () => {
 
   return (
     <div>
-      <Header course = {course} />
+      <Header course = {course.name} />
       <Content />
-      <Total total = {part1 + part2 + part3} />
+      <Total total = {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises} />
     </div>
   )
 }
