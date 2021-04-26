6bc99a3b26250fe0bbe61cd936074c01bdb69222
Pyro-Impact <77907654+Pyro-Impact@users.noreply.github.com>
2021-04-25 20:56:25 -0700

    hku

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -2,68 +2,101 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const Course = (props) => {
+  const coursearr = []
+  for(let i = 0; i < props.course.length; i++) {
+    coursearr.push(<Header course = {props.course[i]}/>)
+    coursearr.push(<Content course = {props.course[i]}/>)
+    coursearr.push(<Total course = {props.course[i]}/>)
+  }
   return(
     <div>
-      <Header course = {props.course}/>
-      <Total course = {props.course}/>
-      <Content course = {props.course}/>
+      {coursearr}
     </div>
   )
 }
 
 const Header = ({ course }) => {
   return (
-    <h1>{course.name}</h1>
-  )
-}
-
-const Total = ({ course }) => {
-  const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
-  return(
-    <p>Number of exercises {sum}</p>
-  ) 
-}
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.part.name} {props.part.exercises}
-    </p>    
-  )
-}
-
-const Content = ({ course }) => {
-  return (
     <div>
-      <Part part={course.parts[0]} />
-      <Part part={course.parts[1]} />
-      <Part part={course.parts[2]} />
+      <h2>Web Development Curriculum</h2>
+      <h3>{course.name}</h3>
     </div>
   )
 }
 
-const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
+const Total = ({ course }) => {
+  const total = course.parts.reduce((sum, order) => 
+    <p key = {order.id}> {sum+ order.exercises} </p>, 0 );
+  return(
+    <ul>{total}</ul>
+  ) 
+}
+
+const Content = ({ course }) => {
+  const contentarr = []
+  for(let i = 0; i < course.parts.length; i++) {
+    contentarr.push(<Part part = {course.parts[i]}/>)
   }
+  return (
+    <div>
+      {contentarr}
+    </div>
+  )
+}
+
+const Part = ({ part }) => {
+  return (
+    <div>
+      {part.name} {part.exercises}
+    </div>    
+  )
+}
+
+const App = () => {
+  const course = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return <Course course={course} />
 }
